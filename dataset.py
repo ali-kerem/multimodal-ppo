276cac9edcd5812c23184326afd3b0f73b0d42bd
ali-kerem <alikerembozkurtt@gmail.com>
2025-06-04 18:41:14 +0300

    Return PIL image, only return question for conversation and add concise answer parameter

--- dataset.py
+++ dataset.py
@@ -1,20 +1,20 @@
 import os
 import json
 from torch.utils.data import Dataset
-
+from PIL import Image
 
 class LLaVACoT(Dataset):
-    def __init__(self, data_path: str):
+    def __init__(self, data_path: str, concise_answer: bool = False):
         self.data_path = data_path
         self.data = self.load_data()
-        self.start_tag = "<CONCLUSION>"
-        self.end_tag = "</CONCLUSION>"
+        self.concise_answer = concise_answer
+        self.single_word_or_phrase = " Answer the question using a single word or phrase."
 
     def load_data(self):
         data = []
         with open(self.data_path, "r") as f:
             for line in f:
-                data.append(json.loads(line)) # Use json.loads() for each line
+                data.append(json.loads(line))
         return data
 
     def __len__(self):
@@ -22,14 +22,18 @@ class LLaVACoT(Dataset):
     
     def __getitem__(self, idx):
         data_dict = self.data[idx]
-        image_path = os.path.join(self.data_path, data_dict["image"])
+        image_path = os.path.join(self.data_path.rsplit("/", 1)[0], "unzipped", data_dict["image"])
+        image = Image.open(image_path)
         conversations = data_dict["conversations"]
+
         for conversation in conversations:
+            if self.concise_answer and conversation["from"] == "human":
+                if self.single_word_or_phrase in conversation["value"]:
+                    conversation["value"] = conversation["value"].replace(self.single_word_or_phrase, "")
             if conversation["from"] == "gpt":
-                no_reasoning_answer = conversation["value"]
-                start_index = no_reasoning_answer.find(self.start_tag) + len(self.start_tag)
-                end_index = no_reasoning_answer.find(self.end_tag)
-                conclusion_text = no_reasoning_answer[start_index:end_index].strip()
-                conversation["value"] = conclusion_text
+                conversations.remove(conversation)
                 
-        return conversations, image_path
+        return {
+            "conversations": conversations,
+            "image": image
+        }
