b7551101b9248744bfe51da6f53224efb70f168e
ali-kerem <alikerembozkurtt@gmail.com>
2025-06-09 14:33:41 +0300

    Adjust the dataset class

--- dataset.py
+++ dataset.py
@@ -4,36 +4,29 @@ from torch.utils.data import Dataset
 from PIL import Image
 
 class LLaVACoT(Dataset):
-    def __init__(self, data_path: str, concise_answer: bool = False):
+    def __init__(self, data_path: str, concise_answer: bool = True):
         self.data_path = data_path
-        self.data = self.load_data()
+        self.load_dataset()
         self.concise_answer = concise_answer
         self.single_word_or_phrase = " Answer the question using a single word or phrase."
 
-    def load_data(self):
-        data = []
+    def load_dataset(self):
         with open(self.data_path, "r") as f:
-            for line in f:
-                data.append(json.loads(line))
-        return data
+            self.data = json.load(f)
 
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, idx):
         data_dict = self.data[idx]
-        image_path = os.path.join(self.data_path.rsplit("/", 1)[0], "unzipped", data_dict["image"])
+        image_path = os.path.join(self.data_path.rsplit("/", 1)[0], data_dict["image"])
         image = Image.open(image_path)
-        conversations = data_dict["conversations"]
 
-        for conversation in conversations:
-            if self.concise_answer and conversation["from"] == "human":
-                if self.single_word_or_phrase in conversation["value"]:
-                    conversation["value"] = conversation["value"].replace(self.single_word_or_phrase, "")
-            if conversation["from"] == "gpt":
-                conversations.remove(conversation)
+        if not self.concise_answer:
+            data_dict["conversations"][0]["content"] = data_dict["conversations"][0]["content"].replace(self.single_word_or_phrase, "")
                 
         return {
-            "conversations": conversations,
-            "image": image
+            "conversations": data_dict["conversations"][:1],
+            "conversations_all": data_dict["conversations"],
+            "image": image,
         }
